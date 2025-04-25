971653e9bbc69db89d13c37392dbc50b65bcf49f
hadiranweb <hadiranweb@gmail.com>
2025-04-25 19:33:40 +0330

    Update seller_handlers.py

--- src/handlers/seller_handlers.py
+++ src/handlers/seller_handlers.py
@@ -56,6 +56,5 @@ def register_handlers(app: Application):
             ADD_CUSTOMER: [MessageHandler(filters.TEXT & ~filters.COMMAND, save_customer)],
         },
         fallbacks=[],
-        per_message=True,
     )
     app.add_handler(conv_handler)
