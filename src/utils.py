a0d6209b97ec359f694d734b74300fd4e755a9cd
hadiranweb <hadiranweb@gmail.com>
2025-04-25 17:42:05 +0330

    Delete src/utils.py

--- src/utils.py
+++
@@ -1,8 +0,0 @@
-def validate_quantity(quantity):
-    try:
-        qty = int(quantity)
-        if qty <= 0:
-            raise ValueError("تعداد باید مثبت باشد!")
-        return qty
-    except ValueError:
-        raise ValueError("تعداد باید عدد باشد!")
