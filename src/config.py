a8379db18945129d3a0932426f4e00a4a275d7ae
hadiranweb <hadiranweb@gmail.com>
2025-04-25 18:02:25 +0330

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,4 +2,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+PORT = int(os.getenv("PORT"))
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not all([BOT_TOKEN, WEBHOOK_URL, DATABASE_URL]):
+    raise ValueError("Missing required environment variables")
