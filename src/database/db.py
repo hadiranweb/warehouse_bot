4597c7fc93dd85256aa2faf0e1bc4fda468f33e9
hadiranweb <hadiranweb@gmail.com>
2025-04-25 18:20:43 +0330

    Update db.py

--- src/database/db.py
+++ src/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
-from models import Base
+from .models import Base  # استفاده از import نسبی
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
