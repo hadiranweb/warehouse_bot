10c1ebce470714a8fe003f1414336fb6dfb8a6ea
hadiranweb <hadiranweb@gmail.com>
2025-04-25 21:58:04 +0330

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,12 +1,7 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-PORT = int(os.getenv("PORT"))
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://warehouse-bot-123.onrender.com")
+PORT = int(os.getenv("PORT", 8000))
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not all([BOT_TOKEN, WEBHOOK_URL, DATABASE_URL]):
-    raise ValueError("Missing required environment variables")
+SECRET_TOKEN = os.getenv("SECRET_TOKEN", "mysecret123")
