0b2d91d7a9812870619aff2bd05a462147a24a9f
hadiranweb <hadiranweb@gmail.com>
2025-04-25 18:10:47 +0330

    Update db.pyظ

--- src/database/db.py
+++ src/database/db.py
@@ -1,1 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URL
+from models import Base
 
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+def get_session():
+    return Session()
